67826eee27f23110650336112fad5f2f18a5f394
최동혁 <Developer5>
2023-02-08 21:03:10 +0900

    페이징 작업중

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,2 +1,3 @@
 import "./src/style/reset.css";
+import "./src/style/common.css";
 import "./src/style/mdx.css";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,64 @@
-import { graphql, PageProps } from "gatsby";
-import React from "react";
+import { graphql, PageProps, useStaticQuery } from "gatsby";
+import React, { useState } from "react";
 import Title from "components/Title";
 import Layout from "components/Layout";
 import MainContent from "components/main/ContentCard";
+import styled from "styled-components";
+
+const PagingBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PageNum = styled.p`
+  padding: 5px;
+  font-weight: 600;
+`;
 
 export default function IndexPage({ data }: PageProps<Queries.PostListQuery>) {
   const {
-    allMdx: { nodes },
+    allMdx: { nodes, totalCount },
   } = data;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPage = Math.floor(312 / 10) + 1;
+  let page = 1;
+
+  const pageClickHandler = (e: any) => {
+    setCurrentPage(e.currentTarget.dataset.value);
+  };
+
+  console.log(nodes);
 
   return (
     <Layout>
       {nodes.map((node) => (
         <MainContent key={node.id} {...node} />
       ))}
+      <PagingBox>
+        {Array.from({ length: maxPage })
+          .fill(1)
+          .map((_, index) => {
+            return (
+              <PageNum
+                dataset-value={index + 1}
+                key={index + 1}
+                className={currentPage === index + 1 ? "active" : ""}
+              >
+                {index + 1}
+              </PageNum>
+            );
+          })}
+      </PagingBox>
     </Layout>
   );
 }
 
 export const query = graphql`
   query PostList {
-    allMdx(limit: 10, sort: { frontmatter: { upload: DESC } }) {
+    allMdx(sort: { frontmatter: { upload: DESC } }) {
       nodes {
         frontmatter {
           category
@@ -32,6 +70,7 @@ export const query = graphql`
         id
         excerpt
       }
+      totalCount
     }
   }
 `;
